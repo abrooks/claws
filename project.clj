349f7690e6af8fabfbaff84b146f96ffce83f50d
Aaron Brooks <aaron.brooks@viasat.com>
2018-05-06 17:00:59 -0400

    Bump Clojure from 1.9.0-alpha15 to 1.9.0.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/abrooks/claws"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [datascript "0.15.5"]
                  [com.rpl/specter "1.0.0"]
                  [org.clojure/data.json "0.2.6"]
